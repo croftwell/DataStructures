1ece75be288aeb5de8095a3e8f31f777fc728b55
Ali Çavuşlu <cavuslumehmet@icloud.com>
2022-12-21 14:29:11 +0300

    sona eleman ekleme işlemi

--- DataStructures/main.c
+++ DataStructures/main.c
@@ -5,10 +5,65 @@
 //  Created by Ali on 21.12.2022.
 //
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+//linked list sona eleman ekleme işlemi
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node* start = NULL;
+struct node* temp = NULL;
+struct node* q = NULL;
+
+
+void sonaEkle(int veri)
+{
+    struct node* eklenecek = (struct node*)malloc(sizeof(struct node));
+    eklenecek -> data = veri;
+    eklenecek -> next = NULL;
+    
+    if(start == NULL)
+    {
+        start = eklenecek;
+    }
+    
+    else {
+        q = start;
+        while(q -> next != NULL)
+        {
+            
+            q = q -> next;
+        }
+        q -> next = eklenecek;
+    }
 }
+
+void yazdir () {
+    
+    q = start;
+    while (q ->next != NULL) {
+        printf("%d =>", q -> data);
+        q = q ->next;
+        
+    }
+    
+    printf("%d", q->data);
+}
+
+int main(int secim)
+{
+    
+    printf("bir tane sayı girin..");
+    scanf("%d", &secim);
+    sonaEkle(secim);
+    yazdir();
+    
+}
+
