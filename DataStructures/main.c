7e5f509a32bf8a4c8379e850296d0a4403e33b60
Ali Çavuşlu <cavuslumehmet@icloud.com>
2022-12-21 14:08:12 +0300

    Initial Commit

---
+++ DataStructures/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DataStructures
+//
+//  Created by Ali on 21.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
